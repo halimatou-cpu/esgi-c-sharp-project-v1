a23e2a0eb2b51fd5af8cec43cb3400ffeaf5a7e7
ahmad2743 <amethdione10@gmail.com>
2022-05-30 18:39:57 +0200

    adding other solution

---
+++ DesignPatternProject/DesignPatternProject/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text;
+
+namespace DesignPatternProject;
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+       // MakeCommandDashbord();
+    }
+
+    public static string MakeCommandDashbord()
+    {
+        string key;
+        do
+        {
+            Console.WriteLine("############### Appuyer sur la touche correspondate pour faire votre commande. ###############");
+            Console.WriteLine("(a) pour Jambon beurre");
+            Console.WriteLine("(b) pour Poulet crudités");
+            Console.WriteLine("(c) pour Dieppois");
+            Console.WriteLine("___________________________________________________________________________________");
+            key = (Console.ReadLine()?.ToString()!);
+        } while (!key.Equals("a") && !key.Equals("b") && !key.Equals("c"));
+
+        return key;
+    }
+
+    public static void CommandChoice(string choice, out SandwichCommand sandwichCommand)
+    {
+        switch (choice)
+        {
+            case "a":
+                
+        }
+    }
+}
